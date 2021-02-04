2375c16156c8b5339ba848d4e167c865f3d24563
Michael Fullom <mfullom@ahfc.us>
2021-02-03 22:40:04 -0900

    dependency build out

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello World!")
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const pg = require('knex')({
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    searchPath: ['knex', 'public'],
+    migrations: {
+        tableName: 'knex_migrations'
+      }
+  });
+ 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
