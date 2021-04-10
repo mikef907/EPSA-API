b89da2b45cd4f00b609a38d54886d48e3d75488b
Michael Fullom <mfullom@ahfc.us>
2021-04-10 10:17:10 -0800

    config update

--- src/config.ts
+++ src/config.ts
@@ -6,5 +6,5 @@ export const IsLive = parseInt(process.env.LIVE as string) === 1;
 export const JwtSignature: string = process.env.JWT_SIG as string;
 export const GmailAppPW: string = process.env.GMAIL_APP_PW as string;
 export const AppRoot = IsLive
-  ? 'https://icy-ground-090caca1e.azurestaticapps.net/'
+  ? 'https://epsa-web.vercel.app/'
   : 'http://localhost:3000/';
